a2e19bfb38da09770e8fdbc75607fc61b0a5a60b
Sunny Goyal <goyalsunny888@gmail.com>
2018-01-06 23:46:11 +0530

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'Sunny', age: 20};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({  // insert lets you insert new document into your collection
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //   // ops attribute stores all the docs that were inserted
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Sunny',
+  //   age: 20,
+  //   location: 'Ghaziabad'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+  // });
+
+  db.close();
+});

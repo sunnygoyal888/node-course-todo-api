c667faf1a1767d80837cfb26a990e377996d40ae
Sunny Goyal <goyalsunny888@gmail.com>
2018-01-07 01:57:14 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5a511f85011556ce2f91c0f3')
+  // }, {
+  //   $set: {   // $set is a mongodb update operator
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5a4e3f1c13061821a41088c3')
+  }, {
+    $set: {
+      name: 'Sunny'
+    }, $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
